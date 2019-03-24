ba503fc3cc72773e78a8ee8eb07dc41d90ecf37f
rami9284 <judramirez@csumb.edu>
2019-03-24 16:09:57 -0700

    printing changes

--- TCPCapitalizationClient.py
+++ TCPCapitalizationClient.py
@@ -11,6 +11,7 @@ import random
 serverName = 'localhost'
 serverPort = 12000
 r1 = random.randint(0,10)
+isFirstX = 0
 
 #Sockets are set for both packets
 clientSocket1 = socket(AF_INET, SOCK_STREAM)
@@ -26,9 +27,11 @@ sentenceY = "Client Y: Bob"
 
 #Sending packets in random order
 if r1%2 == 0:
+    isFirstX = 1
     clientSocket1.send(sentenceX.encode())
     clientSocket2.send(sentenceY.encode())
 else:
+    isFirstX = 0
     clientSocket2.send(sentenceY.encode())
     clientSocket1.send(sentenceX.encode())
 
@@ -39,8 +42,11 @@ modifiedSentenceY = clientSocket2.recv(1024).decode()
 #print statment for message received by server
 print sentenceX + "\n"
 print sentenceY + "\n"
-print modifiedSentenceX + "\n"
-print modifiedSentenceY + "\n"
+
+if(isFirstX == 1):
+    print modifiedSentenceX + "\n"
+else:
+    print modifiedSentenceY + "\n"
 
 #Closing the connection
 clientSocket1.close()
